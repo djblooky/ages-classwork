047e8ae93698d678d32b4fb5a2c03831e2879bf8
Charlie Phillips <hi.charliephillips@gmail.com>
2020-09-24 22:05:05 -0500

    change physics material based on movein or stopping

---
+++ Week1_ThirdPersonDesign_CharliePhillips/Assets/Scripts/RigidbodyCharacterController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class RigidbodyCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float accelerationForce = 10;
+
+    [SerializeField]
+    private float maxSpeed = 2;
+
+    [SerializeField]
+    private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
+
+    private new Rigidbody rigidbody;
+    private Vector2 input;
+    private new Collider collider;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        var inputDirection = new Vector3(input.x, 0, input.y);
+
+        collider.material = inputDirection.magnitude > 0 ?  movingPhysicsMaterial : stoppingPhysicsMaterial;
+
+        /*
+        if(inputDirection.magnitude > 0)
+        {
+            collider.material = movingPhysicsMaterial;
+        }
+        else
+        {
+            collider.material = stoppingPhysicsMaterial;
+        }*/
+
+        if(rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
+        }
+
+    }
+
+    private void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+    }
+}
