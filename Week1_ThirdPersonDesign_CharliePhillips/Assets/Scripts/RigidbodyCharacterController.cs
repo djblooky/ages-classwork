5b1762600c2492d9b6b4feb461e6e96a3ec7b5b1
Charlie Phillips <hi.charliephillips@gmail.com>
2020-09-30 14:38:08 -0500

    camera-based player movement

--- Week1_ThirdPersonDesign_CharliePhillips/Assets/Scripts/RigidbodyCharacterController.cs
+++ Week1_ThirdPersonDesign_CharliePhillips/Assets/Scripts/RigidbodyCharacterController.cs
@@ -9,11 +9,17 @@ public class RigidbodyCharacterController : MonoBehaviour
     private float maxSpeed = 2;
 
     [SerializeField]
+    [Tooltip("How fast the player turns, 0 = no turning, 1 = instant turning")]
+    [Range(0,1)]
+    private float turnSpeed = 0.1f;
+
+    [SerializeField]
     private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
 
     private new Rigidbody rigidbody;
     private Vector2 input;
     private new Collider collider;
+    
 
     private void Start()
     {
@@ -23,25 +29,29 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        /// Move player relative to camera
         var inputDirection = new Vector3(input.x, 0, input.y);
+
+        Vector3 flattenedCameraForward = Camera.main.transform.forward;
+        flattenedCameraForward.y = 0; //get rid of the y component of the vector - stops player from moving up or down
+        var CameraRotation = Quaternion.LookRotation(flattenedCameraForward);
+
+        Vector3 cameraRelativeInputDirection = CameraRotation * inputDirection;
 
         collider.material = inputDirection.magnitude > 0 ?  movingPhysicsMaterial : stoppingPhysicsMaterial;
 
-        /*
-        if(inputDirection.magnitude > 0)
-        {
-            collider.material = movingPhysicsMaterial;
-        }
-        else
-        {
-            collider.material = stoppingPhysicsMaterial;
-        }*/
-
         if(rigidbody.velocity.magnitude < maxSpeed)
         {
-            rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
+            rigidbody.AddForce(cameraRelativeInputDirection * accelerationForce, ForceMode.Acceleration);
         }
 
+        //rotate player relative to camera
+        if (cameraRelativeInputDirection.magnitude > 0) //the player is pressing input keys
+        {
+            var targetRotation = Quaternion.LookRotation(cameraRelativeInputDirection);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
+        }
+       
     }
 
     private void Update()
