a5b9bc421b9e09aa678723ece0f470d412ca7b21
Charlie Phillips <hi.charliephillips@gmail.com>
2020-10-08 00:11:54 -0500

    added fade out

--- Week1_ThirdPersonDesign_CharliePhillips/Assets/Scripts/ContextualMessageController.cs
+++ Week1_ThirdPersonDesign_CharliePhillips/Assets/Scripts/ContextualMessageController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ContextualMessageController : MonoBehaviour
 {
+    [SerializeField]
+    private float fadeOutDuration = 1;
+
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
 
@@ -18,12 +21,23 @@ public class ContextualMessageController : MonoBehaviour
     {
         canvasGroup.alpha = 1;
         messageText.text = message;
+        //wait for duration
         yield return new WaitForSeconds(messageDuration);
+        //start fading out
+        float fadeElapsedTime = 0;
+        float fadeStartTime = Time.time;
+        while (fadeElapsedTime < fadeOutDuration)
+        {
+            fadeElapsedTime = Time.time - fadeStartTime; //use Time.time in coroutine 
+            canvasGroup.alpha = 1 - fadeElapsedTime / fadeOutDuration;
+            yield return null;
+        }
         canvasGroup.alpha = 0;
     }
 
     private void OnMessageTriggerEntered(string message, float messageDuration)
     {
+        StopAllCoroutines();
         StartCoroutine(ShowMessage(message, messageDuration));
     }
 
