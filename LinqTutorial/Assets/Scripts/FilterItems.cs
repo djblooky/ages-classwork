f109b5adcf6c55ae697d427e04a33edbb7f19995
Charlie Phillips <hi.charliephillips@gmail.com>
2020-12-13 20:32:56 -0600

    completed linq tutorial

---
+++ LinqTutorial/Assets/Scripts/FilterItems.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public string Name;
+    public int ID;
+    public int Buff;
+}
+
+public class FilterItems : MonoBehaviour
+{
+    public List<Item> items;
+
+    private void Start()
+    {
+        //see if item 3 exists
+        var result = items.Any(item => item.ID == 3);
+        Debug.Log("Item 3 exists? " + result);
+
+        //print all items with buff > 20
+        var newList = items.Where(item => item.Buff > 20);
+
+        Debug.Log("Items with buff > 20");
+        foreach (Item item in newList)
+        {
+            Debug.Log(item.Name);
+        }
+
+        //calculate the average of all buff stats
+        var average = items.Average(item => item.Buff);
+        Debug.Log("Average: " + average);
+
+    }
+}
---
+++ LinqTutorial/Assets/Scripts/QuizGrades.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class QuizGrades : MonoBehaviour
+{
+    private int[] quizGrades = { 35, 99, 100, 45, 67, 80, 89, 78 };
+
+    private void Start()
+    {
+        var passingGrades = quizGrades.Where(qg => qg > 69) //saves all passing grades into a collection 
+            .OrderByDescending(g => g).Reverse(); //orders the list by ascending 
+
+        Debug.Log("Passing grades:");
+        foreach (var grade in passingGrades)
+        {
+            Debug.Log(grade);
+        }
+    }
+}
